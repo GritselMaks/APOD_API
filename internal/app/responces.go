acaf9ac6f525955b3f01adf360d54da2eda2ebe0
GritselMaks <Gritsel.maks@gmail.com>
2022-12-06 11:14:59 +0100

    fix panic(err)

--- internal/app/responces.go
+++ internal/app/responces.go
@@ -2,9 +2,16 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+// jsonErrTmpl is the template to use when returning a JSON error. It is
+// rendered using Printf, not json.Encode, so values must be escaped by the
+// caller.
+const jsonErrTmpl = `{"error":"%s"}`
+
+// Standart form for all responses
 type Response struct {
 	Data interface{} `json:"data"`
 }
@@ -13,6 +20,7 @@ func NewResponse(data interface{}) *Response {
 	return &Response{data}
 }
 
+// Standart form for all Error responses
 type ErrorResponse struct {
 	ErrorMsg string `json:"error"`
 }
@@ -21,31 +29,39 @@ func NewErrorResponse(msg string) *ErrorResponse {
 	return &ErrorResponse{ErrorMsg: msg}
 }
 
-// Forms a response with http Status and data
+// Response with http Status and data
 func ResponseWithJSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	payload := NewResponse(data)
 	response, err := json.Marshal(payload)
 	if err != nil {
-		//FIXME
-		panic(err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, jsonErrTmpl, http.StatusText(http.StatusInternalServerError))
+		return
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(statusCode)
 	w.Write(response)
 }
 
+//Response with http Status and Error
 func RespondWithError(w http.ResponseWriter, statusCode int, msg string) {
 	payload := NewErrorResponse(msg)
 	response, err := json.Marshal(payload)
 	if err != nil {
-		//FIXME
-		panic(err)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, jsonErrTmpl, http.StatusText(http.StatusInternalServerError))
+			return
+		}
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(statusCode)
 	w.Write(response)
 }
 
+//Response with http Status and media content
 func RespondWithPicture(w http.ResponseWriter, statusCode int, data []byte) {
 	w.WriteHeader(statusCode)
 	w.Header().Set("Content-Type", "application/octet-stream")
